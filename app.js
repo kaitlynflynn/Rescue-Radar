ae58f95536e90860d538fb20f82aea69d788c868
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-02 14:14:32 -0700

    Setup route handler and views; installed pug

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-/**
- * app.js
- */
+import path from 'path';
 import express from 'express';
+import routes from './routes/index';
+
 // Express app setup
 const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// use routes
+app.use('/', routes);
+
 export default app;
