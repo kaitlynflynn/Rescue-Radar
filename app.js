cb065722face158e73e1d3e595cb691def4e0b2f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-02 14:56:43 -0700

    Installed Morgan dependency; modified app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import path from 'path';
 import express from 'express';
+import dotenv from 'dotenv';
 import routes from './routes/index';
+
+// use dotenv
+dotenv.config({
+  silent: true,
+});
 
 // Express app setup
 const app = express();
@@ -9,8 +15,18 @@ const app = express();
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+
 // use routes
-app.use('/', routes);
+app.use('/', routes); // <-- error 404 handler comes after this
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 app.use((err, req, res, next) => {
