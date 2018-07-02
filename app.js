ee518f1d1519969d084a0076ad85777690abd7ac
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-02 15:14:47 -0700

    Cleaned up code in app.js

--- app.js
+++ app.js
@@ -2,46 +2,35 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
-import routes from './routes/index';
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import routes from './routes';
 // use dotenv
 dotenv.config({
   silent: true,
 });
-
 // Express app setup
 const app = express();
-
 // view engine
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
-
-// serve static files from 'public'
-app.use(express.static(path.join(__dirname, './public')));
-
-// use routes
-app.use('/', routes); // <-- error 404 handler comes after this
-
 // logger
 app.use(logger('combined'));
-
 // body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // cookie parser
 app.use(cookieParser());
-
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+// use routes
+app.use('/', routes);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 // error handlers
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
@@ -51,5 +40,4 @@ app.use((err, req, res, next) => {
   });
   next();
 });
-
 export default app;
